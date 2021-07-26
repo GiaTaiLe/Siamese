fa71d2215e8e8d99429c198196612d7df3eecda5
GiaTaiLe <51397531+GiaTaiLe@users.noreply.github.com>
2021-07-27 02:40:40 +0900

    Add files via upload

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import numpy as np
 import torchvision.datasets as dset
 
 from folder import ImageFolderCustom
+from utils import TwoCropsTransform, GaussianBlur
 
 class prepared_dataset(torch.utils.data.Dataset):
 	def __init__(self, dataset, in_class):
@@ -24,17 +25,27 @@ def data_load(in_class = None, train = True, in_or_out = "in"):
 			'dragonfly', 'dumbbell', 'forklift', 'goblet', 'grand_piano', 'hotdog', 'hourglass', 'manhole_cover',
 			'mosque', 'nail', 'parking_meter', 'pillow', 'revolver', 'rotary_dial_telephone', 'schooner', 'snowmobile',
 			'soccer_ball', 'stingray', 'strawberry', 'tank', 'toaster', 'volcano']
-	normalize_transform = trn.Compose([trn.Resize(256), trn.CenterCrop(224),
-                               trn.ToTensor(), trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+	normalize = trn.Normalize(mean=[0.485, 0.456, 0.406],
+		std=[0.229, 0.224, 0.225])
+	augmentation = [trn.RandomResizeCrop(224, scale=(0.2, 1.)),
+				trn.RandomApply([trn.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+				trn.RandomGrayscale(p=0.2),
+				trn.RandomApply([GaussianBlur(0.1,0.2)], p=0.5),
+				trn.RandomHorizontalFlip(),
+				trn.ToTensor(),
+				normalize]
+
+	aug_transform = TwoCropsTransform(trn.Compose(augmentation))
+
 	if train:
 		path = "/home/giatai/Documents/Python/data/ImageNet_30classes/one_class_train/" + classes[in_class]
-		data_load = dset.ImageFolder(path, transform = normalize_transform)
+		data_load = dset.ImageFolder(path, transform = aug_transform)
 	elif not train:
 		path = "/home/giatai/Documents/Python/data/ImageNet_30classes/one_class_test/"
 		if in_or_out == "out":
-			data_load = ImageFolderCustom(path, transform = normalize_transform, remove_classes = classes[in_class])
+			data_load = ImageFolderCustom(path, transform = aug_transform, remove_classes = classes[in_class])
 		elif in_or_out == "in":
 			path = path + classes[in_class]
-			data_load = ImageFolderCustom(path, transform = normalize_transform)
+			data_load = ImageFolderCustom(path, transform = aug_transform)
 
 	return prepared_dataset(data_load, in_class)
--- train.py
+++ train.py
@@ -17,20 +17,28 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import cv2 as cv
-from model.resnet import resnet18
+
+from model.simsiam import SimSiam
+from data import data_load
 
 parser = argparse.ArgumentParser(description = "Train one-class model - ImageNet",
 	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--in_class', '-in', type=int, default=2, help='Class to have as the target/in distribution.')
+parser.add_argument("--dim", type=int, default=2048, help="dimension of the presentation and projection")
+parser.add_argument("--pred_dim", type=int, default=512, help="dimension of the bottleneck of mlp prediction")
 #Optimization options
 parser.add_argument('--batch_size', '-b', type=int, default=32, help='Batch size.')
 parser.add_argument('--epochs', '-e', type=int, default=20, help='Number of epochs to train.')
 parser.add_argument('--learning_rate', '-lr', type=float, default=0.1, help='The initial learning rate.')
 parser.add_argument('--momentum', type=float, default=0.9, help='Momentum.')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
 
 #Checkpoints
 parser.add_argument('--save', '-s', type=str, default='snapshots/ood', help='Folder to save checkpoints.')
-parser.add_argument('--load', '-l', type=str, default='', help='Checkpoint path to resume / test.')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
 parser.add_argument('--test', '-t', action='store_true', help='Test only flag.')
 
 args = parser.parse_args()
@@ -72,8 +80,68 @@ def train(net, train_loader, optimizer, criterion, epoch, args):
 		if i%100 == 0:
 			progress.display(i)
 
-def main():
+def save_ckpt(state, is_best, filename="ckpt.pth.tar"):
+	torch.save(state, filename)
+	if is_best:
+		shutil.copyfile(filename, "model_best.pth.tar")
+
+def main_worker(args):
+	net = SimSiam(args.dim, args.pred_dim)
+
+	#net = torch.nn.SyncBatchNorm.convert_sync_batchnorm(net)
+
+	#infer learning rate before changing batchsize
+	init_lr = args.learning_rate*args.batch_size/256
+
+	net.cuda()
+	#net = nn.parallel.DistributedDataParallel(net)
+	#print(net)
 	
+	criterion = nn.CosineSimilarity(dim=1).cuda()
+
+	optim_params = net.parameters()
+
+	optimizer = torch.optim.SGD(optim_params, init_lr,
+		momentum=args.momentum,
+		weight_decay=args.weight_decay)
+
+	#optionally resume from a ckpt
+	if args.resume:
+		if os.path.isfile(args.resume):
+			print("=> loading checkpoint '{}'".format(args.resume))
+			ckpt = torch.load(args.resume)
+			args.start_epoch = ckpt["epoch"]
+			net.load_state_dict(ckpt["state_dict"])
+			optimizer.load_state_dict(ckpt["optimizer"])
+			print("=> loaded checkpoint '{}' (epoch {})"
+				.format(args.resume, checkpoint['epoch']))
+		else:
+			print("=> no checkpoint found at '{}'".format(args.resume))
+
+	cudnn.benchmark = True
+
+	#data loading code
+	traindir = os.path.join(args.data, "train")
+
+	train_dataset = data_load(in_class = args.in_class, train = True, in_or_out = "in")
+	#train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+
+	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+		num_workers=4, pin_memory=True)
+
+	#train for one epoch
+	for epoch in range(args.epochs)
+		train(net, train_loader, optimizer, criterion, epoch, args)
+
+	save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': "resnet50",
+                'state_dict': model.state_dict(),
+                'optimizer' : optimizer.state_dict(),
+            }, is_best=False, filename='checkpoint_{:04d}.pth.tar'.format(epoch))
+
+def main():
+
 
 class AverageMeter(object):
 	def __init__(self, name, fmt=":f"):
